cd6eba891177106490a4082334467f583df318ba
pascalpoizat <pascal.poizat@lip6.fr>
2018-10-10 15:36:23 +0200

    test: add initial tests

--- src/Fbpmn.hs
+++ src/Fbpmn.hs
@@ -1,7 +1,10 @@
 module Fbpmn
        ( someFunc
+       , add
        ) where
 
 someFunc :: IO ()
 someFunc = putStrLn ("fBPMN: formal tools for BPMN" :: Text)
 
+add :: Int -> Int -> Int
+add x y = x + y
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,30 @@
-main :: IO ()
-main = putStrLn ("Test suite not yet implemented" :: String)
+import           Test.Tasty
+import           Test.Tasty.HUnit
+-- import           Test.Tasty.QuickCheck as QC
+-- import           Test.Tasty.SmallCheck as SC
+import           Test.Tasty.Runners.Html
 
+import Fbpmn
+
+main :: IO ()
+main = defaultMainWithIngredients (htmlRunner : defaultIngredients) test
+
+test :: TestTree
+test = testGroup "Tests" [mainTests]
+
+mainTests :: TestTree
+mainTests = testGroup "Main tests" [unittests]
+
+unittests :: TestTree
+unittests = testGroup "Unit tests" [uDummy]
+
+uDummy :: TestTree
+uDummy = testGroup "Unit tests for add"
+  [
+    testCase "add 0/0" $
+    add 0 0 @?= 0
+  , testCase "add 0/n" $
+    add 0 2 @?= 2
+  , testCase "add n/m" $
+    add 2 3 @?= 5
+  ]
