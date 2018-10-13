08cb6b44950f785e18e5d9351cb7fbcc86a2130d
pascalpoizat <pascal.poizat@lip6.fr>
2018-10-13 19:01:36 +0200

    feat: add first set of BPMN constructs

--- src/Fbpmn.hs
+++ src/Fbpmn.hs
@@ -1,10 +1,100 @@
-module Fbpmn
-       ( someFunc
-       , add
-       ) where
+module Fbpmn where
 
 someFunc :: IO ()
 someFunc = putStrLn ("fBPMN: formal tools for BPMN" :: Text)
 
-add :: Int -> Int -> Int
-add x y = x + y
+--
+-- Node types
+--
+data NodeType = AbstractTask
+              | SendTask
+              | ReceiveTask
+              | SubProcess
+              | XorGateway
+              | OrGateway
+              | AndGateway
+              | NoneStartEvent
+              | NoneEndEvent
+  deriving (Eq)
+
+--
+-- Edge types
+--
+data EdgeType = NormalSequenceFlow
+              | ConditionalSequenceFlow
+              | DefaultSequenceFlow
+              | MessageFlow
+  deriving (Eq)
+
+--
+-- Messages
+--
+type Message = Text
+
+--
+-- Processes
+--
+type Process = Int
+
+--
+-- XML Ids
+--
+type Id = Text
+
+--
+-- Nodes
+--
+type Node = Id
+
+--
+-- Edges
+--
+type Edge = Id
+
+--
+-- BPMN Graph
+--
+data BpmnGraph = BpmnGraph { nodes :: [Node]
+                           , edges :: [Edge]
+                           , catN :: Node -> Maybe NodeType
+                           , catE :: Edge -> Maybe EdgeType
+                           , sourceE :: Edge -> Maybe Node
+                           , targetE :: Edge -> Maybe Node
+}
+
+--
+-- nodesT
+--
+nodesT :: BpmnGraph -> NodeType -> [Node]
+nodesT g t = [ n | n <- nodes g, cat n == Just t ] where cat = catN g
+
+--
+-- nodesE
+--
+edgesT :: BpmnGraph -> EdgeType -> [Edge]
+edgesT g t = [ e | e <- edges g, cat e == Just t ] where cat = catE g
+
+--
+-- in
+--
+inN :: BpmnGraph -> Node -> [Edge]
+inN g n = [ e | e <- edges g, target e == Just n ] where target = targetE g
+
+
+--
+-- out
+--
+outN :: BpmnGraph -> Node -> [Edge]
+outN g n = [ e | e <- edges g, source e == Just n ] where source = sourceE g
+
+--
+-- inT
+--
+inT :: BpmnGraph -> Node -> EdgeType -> [Edge]
+inT g n t = [ e1 | e1 <- edgesT g t, e2 <- inN g n, e1 == e2]
+
+--
+-- outT
+--
+outT :: BpmnGraph -> Node -> EdgeType -> [Edge]
+outT g n t = [ e1 | e1 <- edgesT g t, e2 <- outN g n, e1 == e2]
--- test/Spec.hs
+++ test/Spec.hs
@@ -16,15 +16,92 @@ mainTests :: TestTree
 mainTests = testGroup "Main tests" [unittests]
 
 unittests :: TestTree
-unittests = testGroup "Unit tests" [uDummy]
+unittests = testGroup "Unit tests" [uNodesT, uEdgesT, uInN, uOutN]
 
-uDummy :: TestTree
-uDummy = testGroup "Unit tests for add"
-  [
-    testCase "add 0/0" $
-    add 0 0 @?= 0
-  , testCase "add 0/n" $
-    add 0 2 @?= 2
-  , testCase "add n/m" $
-    add 2 3 @?= 5
+uNodesT :: TestTree
+uNodesT = testGroup
+  "Unit tests for nodesT"
+  [ testCase "empty" $ nodesT g1 SendTask @?= []
+  , testCase "non empty, direct"
+  $   nodesT g1 AbstractTask
+  @?= ["T1a", "T1b", "T2a", "T2b"]
   ]
+
+uEdgesT :: TestTree
+uEdgesT = testGroup
+  "Unit tests for edgesT"
+  [ testCase "empty" $ edgesT g1 MessageFlow @?= []
+  , testCase "non empty, direct"
+  $   edgesT g1 NormalSequenceFlow
+  @?= ["e1","es+a","es+b","e2a","e2b","ej+a","ej+b","e3"]
+  ]
+
+uInN :: TestTree
+uInN = testGroup
+  "Unit tests for inN"
+  [ testCase "empty" $ inN g1 "Start" @?= []
+  , testCase "non empty" $ inN g1 "JoinAnd" @?= ["ej+a", "ej+b"]
+  ]
+
+uOutN :: TestTree
+uOutN = testGroup
+  "Unit tests for outN"
+  [ testCase "empty" $ outN g1 "End" @?= []
+  , testCase "non empty" $ outN g1 "SplitAnd" @?= ["es+a", "es+b"]
+  ]
+
+--
+-- g1
+--
+-- NSE -> + {T1, T2} + -> NEE
+--
+g1 :: BpmnGraph
+g1 = BpmnGraph
+  ["Start", "SplitAnd", "T1a", "T1b", "T2a", "T2b", "JoinAnd", "End"]
+  ["e1", "es+a", "es+b", "e2a", "e2b", "ej+a", "ej+b", "e3"]
+  catN
+  catE
+  source
+  target
+ where
+  catN n = case n of
+    "Start"    -> Just NoneStartEvent
+    "SplitAnd" -> Just AndGateway
+    "T1a"      -> Just AbstractTask
+    "T2a"      -> Just AbstractTask
+    "T1b"      -> Just AbstractTask
+    "T2b"      -> Just AbstractTask
+    "JoinAnd"  -> Just AndGateway
+    "End"      -> Just NoneEndEvent
+    _          -> Nothing
+  catE e = case e of
+    "e1"   -> Just NormalSequenceFlow
+    "es+a" -> Just NormalSequenceFlow
+    "es+b" -> Just NormalSequenceFlow
+    "e2a"  -> Just NormalSequenceFlow
+    "e2b"  -> Just NormalSequenceFlow
+    "ej+a" -> Just NormalSequenceFlow
+    "ej+b" -> Just NormalSequenceFlow
+    "e3"   -> Just NormalSequenceFlow
+    _      -> Nothing
+  source e = case e of
+    "e1"   -> Just "Start"
+    "es+a" -> Just "SplitAnd"
+    "es+b" -> Just "SplitAnd"
+    "e2a"  -> Just "T1a"
+    "e2b"  -> Just "T1b"
+    "ej+a" -> Just "T2a"
+    "ej+b" -> Just "T2b"
+    "e3"   -> Just "JoinAnd"
+    _      -> Nothing
+  target e = case e of
+    "e1"   -> Just "SplitAnd"
+    "es+a" -> Just "T1a"
+    "es+b" -> Just "T1b"
+    "e2a"  -> Just "T2a"
+    "e2b"  -> Just "T2b"
+    "ej+a" -> Just "JoinAnd"
+    "ej+b" -> Just "JoinAnd"
+    "e3"   -> Just "End"
+    _      -> Nothing
+
