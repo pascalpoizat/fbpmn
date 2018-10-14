62bbb23c8348772ce1d19f5e5dd0d55e556daca7
pascalpoizat <pascal.poizat@lip6.fr>
2018-10-14 19:24:32 +0200

    test: add tests for isValidGraph

--- test/Spec.hs
+++ test/Spec.hs
@@ -17,7 +17,17 @@ mainTests :: TestTree
 mainTests = testGroup "Main tests" [unittests]
 
 unittests :: TestTree
-unittests = testGroup "Unit tests" [uNodesT, uEdgesT, uInN, uOutN]
+unittests =
+  testGroup "Unit tests" [uIsValidGraph, uNodesT, uEdgesT, uInN, uOutN]
+
+uIsValidGraph :: TestTree
+uIsValidGraph = testGroup
+  "Unit tests for isValidGraph"
+  [ testCase "missing catN" $ isValidGraph g0a @?= False
+  , testCase "missing catE" $ isValidGraph g0b @?= False
+  , testCase "missing sourceE" $ isValidGraph g0c @?= False
+  , testCase "missing targetE" $ isValidGraph g0d @?= False
+  , testCase "all ok" $ isValidGraph g1 @?= True]
 
 uNodesT :: TestTree
 uNodesT = testGroup
@@ -50,3 +60,216 @@ uOutN = testGroup
   [ testCase "empty" $ outN g1 "End" @?= []
   , testCase "non empty" $ outN g1 "SplitAnd" @?= ["es+a", "es+b"]
   ]
+
+--
+-- graphs
+--
+
+g0a :: BpmnGraph
+g0a = mkGraph
+  ["Start", "SplitAnd", "T1a", "T1b", "T2a", "T2b", "JoinAnd", "End"]
+  ["e1", "es+a", "es+b", "e2a", "e2b", "ej+a", "ej+b", "e3"]
+  catN
+  catE
+  source
+  target
+  name
+ where
+  catN = fromList
+    [ ("Start"   , NoneStartEvent)
+    , ("SplitAnd", AndGateway)
+    , ("T1a"     , AbstractTask)
+    , ("T2a"     , AbstractTask)
+    , ("T1b"     , AbstractTask)
+    , ("T2b"     , AbstractTask)
+--    , ("JoinAnd" , AndGateway)
+    , ("End"     , NoneEndEvent)
+    ]
+  catE = fromList
+    [ ("e1"  , NormalSequenceFlow)
+    , ("es+a", NormalSequenceFlow)
+    , ("es+b", NormalSequenceFlow)
+    , ("e2a" , NormalSequenceFlow)
+    , ("e2b" , NormalSequenceFlow)
+    , ("ej+a", NormalSequenceFlow)
+    , ("ej+b", NormalSequenceFlow)
+    , ("e3"  , NormalSequenceFlow)
+    ]
+  source = fromList
+    [ ("e1"  , "Start")
+    , ("es+a", "SplitAnd")
+    , ("es+b", "SplitAnd")
+    , ("e2a" , "T1a")
+    , ("e2b" , "T1b")
+    , ("ej+a", "T2a")
+    , ("ej+b", "T2b")
+    , ("e3"  , "JoinAnd")
+    ]
+  target = fromList
+    [ ("e1"  , "SplitAnd")
+    , ("es+a", "T1a")
+    , ("es+b", "T1b")
+    , ("e2a" , "T2a")
+    , ("e2b" , "T2b")
+    , ("ej+a", "JoinAnd")
+    , ("ej+b", "JoinAnd")
+    , ("e3"  , "End")
+    ]
+  name = fromList []
+
+g0b :: BpmnGraph
+g0b = mkGraph
+  ["Start", "SplitAnd", "T1a", "T1b", "T2a", "T2b", "JoinAnd", "End"]
+  ["e1", "es+a", "es+b", "e2a", "e2b", "ej+a", "ej+b", "e3"]
+  catN
+  catE
+  source
+  target
+  name
+ where
+  catN = fromList
+    [ ("Start"   , NoneStartEvent)
+    , ("SplitAnd", AndGateway)
+    , ("T1a"     , AbstractTask)
+    , ("T2a"     , AbstractTask)
+    , ("T1b"     , AbstractTask)
+    , ("T2b"     , AbstractTask)
+    , ("JoinAnd" , AndGateway)
+    , ("End"     , NoneEndEvent)
+    ]
+  catE = fromList
+    [ ("e1"  , NormalSequenceFlow)
+    , ("es+a", NormalSequenceFlow)
+    , ("es+b", NormalSequenceFlow)
+    , ("e2a" , NormalSequenceFlow)
+    , ("e2b" , NormalSequenceFlow)
+--    , ("ej+a", NormalSequenceFlow)
+    , ("ej+b", NormalSequenceFlow)
+    , ("e3"  , NormalSequenceFlow)
+    ]
+  source = fromList
+    [ ("e1"  , "Start")
+    , ("es+a", "SplitAnd")
+    , ("es+b", "SplitAnd")
+    , ("e2a" , "T1a")
+    , ("e2b" , "T1b")
+    , ("ej+a", "T2a")
+    , ("ej+b", "T2b")
+    , ("e3"  , "JoinAnd")
+    ]
+  target = fromList
+    [ ("e1"  , "SplitAnd")
+    , ("es+a", "T1a")
+    , ("es+b", "T1b")
+    , ("e2a" , "T2a")
+    , ("e2b" , "T2b")
+    , ("ej+a", "JoinAnd")
+    , ("ej+b", "JoinAnd")
+    , ("e3"  , "End")
+    ]
+  name = fromList []
+
+g0c :: BpmnGraph
+g0c = mkGraph
+  ["Start", "SplitAnd", "T1a", "T1b", "T2a", "T2b", "JoinAnd", "End"]
+  ["e1", "es+a", "es+b", "e2a", "e2b", "ej+a", "ej+b", "e3"]
+  catN
+  catE
+  source
+  target
+  name
+ where
+  catN = fromList
+    [ ("Start"   , NoneStartEvent)
+    , ("SplitAnd", AndGateway)
+    , ("T1a"     , AbstractTask)
+    , ("T2a"     , AbstractTask)
+    , ("T1b"     , AbstractTask)
+    , ("T2b"     , AbstractTask)
+    , ("JoinAnd" , AndGateway)
+    , ("End"     , NoneEndEvent)
+    ]
+  catE = fromList
+    [ ("e1"  , NormalSequenceFlow)
+    , ("es+a", NormalSequenceFlow)
+    , ("es+b", NormalSequenceFlow)
+    , ("e2a" , NormalSequenceFlow)
+    , ("e2b" , NormalSequenceFlow)
+    , ("ej+a", NormalSequenceFlow)
+    , ("ej+b", NormalSequenceFlow)
+    , ("e3"  , NormalSequenceFlow)
+    ]
+  source = fromList
+    [ ("e1"  , "Start")
+    , ("es+a", "SplitAnd")
+    , ("es+b", "SplitAnd")
+    , ("e2a" , "T1a")
+    , ("e2b" , "T1b")
+--    , ("ej+a", "T2a")
+    , ("ej+b", "T2b")
+    , ("e3"  , "JoinAnd")
+    ]
+  target = fromList
+    [ ("e1"  , "SplitAnd")
+    , ("es+a", "T1a")
+    , ("es+b", "T1b")
+    , ("e2a" , "T2a")
+    , ("e2b" , "T2b")
+    , ("ej+a", "JoinAnd")
+    , ("ej+b", "JoinAnd")
+    , ("e3"  , "End")
+    ]
+  name = fromList []
+
+g0d :: BpmnGraph
+g0d = mkGraph
+  ["Start", "SplitAnd", "T1a", "T1b", "T2a", "T2b", "JoinAnd", "End"]
+  ["e1", "es+a", "es+b", "e2a", "e2b", "ej+a", "ej+b", "e3"]
+  catN
+  catE
+  source
+  target
+  name
+ where
+  catN = fromList
+    [ ("Start"   , NoneStartEvent)
+    , ("SplitAnd", AndGateway)
+    , ("T1a"     , AbstractTask)
+    , ("T2a"     , AbstractTask)
+    , ("T1b"     , AbstractTask)
+    , ("T2b"     , AbstractTask)
+    , ("JoinAnd" , AndGateway)
+    , ("End"     , NoneEndEvent)
+    ]
+  catE = fromList
+    [ ("e1"  , NormalSequenceFlow)
+    , ("es+a", NormalSequenceFlow)
+    , ("es+b", NormalSequenceFlow)
+    , ("e2a" , NormalSequenceFlow)
+    , ("e2b" , NormalSequenceFlow)
+    , ("ej+a", NormalSequenceFlow)
+    , ("ej+b", NormalSequenceFlow)
+    , ("e3"  , NormalSequenceFlow)
+    ]
+  source = fromList
+    [ ("e1"  , "Start")
+    , ("es+a", "SplitAnd")
+    , ("es+b", "SplitAnd")
+    , ("e2a" , "T1a")
+    , ("e2b" , "T1b")
+    , ("ej+a", "T2a")
+    , ("ej+b", "T2b")
+    , ("e3"  , "JoinAnd")
+    ]
+  target = fromList
+    [ ("e1"  , "SplitAnd")
+    , ("es+a", "T1a")
+    , ("es+b", "T1b")
+    , ("e2a" , "T2a")
+    , ("e2b" , "T2b")
+--    , ("ej+a", "JoinAnd")
+    , ("ej+b", "JoinAnd")
+    , ("e3"  , "End")
+    ]
+  name = fromList []
+
