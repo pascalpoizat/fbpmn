1008ceeb3b5a0ec999d1c301501d6fcf49209f28
pascalpoizat <pascal.poizat@lip6.fr>
2019-03-03 01:13:50 +0100

    feat: BPMN parsing extended to get message names

--- src/Fbpmn/IO/Bpmn.hs
+++ src/Fbpmn/IO/Bpmn.hs
@@ -11,36 +11,58 @@ import           System.IO.Error                ( IOError
                                                 , isDoesNotExistError
                                                 )
 
-uri :: Text
+uri :: String
 uri = "http://www.omg.org/spec/BPMN/20100524/MODEL"
 
+nA :: String -> QName
+nA s = QName s Nothing Nothing
+
+nE :: String -> QName
+nE s = QName s (Just uri) (Just "bpmn")
+
 nId :: QName
-nId = QName "id" Nothing Nothing
+nId = nA "id"
+
+nName :: QName
+nName = nA "name"
 
 nCollaboration :: QName
-nCollaboration = QName "collaboration" (Just $ toString uri) (Just "bpmn")
+nCollaboration = nE "collaboration"
+
+nParticipant :: QName
+nParticipant = nE "participant"
+
+nMessageFlow :: QName
+nMessageFlow = nE "messageFlow"
 
 {-|
-An experimental BPMN reading (searches the id of the first collaboration)
+An experimental BPMN reading.
+
+Enhancements:
+- remove duplicate message flow names
+- use messages instance of message flow names (if available)
 -}
 decode :: [Content] -> Maybe BpmnGraph
 decode cs = do
-  c <- listToMaybe
-    $ concatMap (findElements nCollaboration) (onlyElems cs)
-  n <- findAttr nId c
+  es <- pure $ onlyElems cs
+  c  <- listToMaybe $ concatMap (findElements nCollaboration) es
+  n  <- findAttr nId c
+  ps <- pure $ findElements nParticipant c
+  mfs <- pure $ findElements nMessageFlow c
+  ms <- sequence $ findAttr nName <$> mfs
   Just
     (mkGraph (toText n)
-             []
-             []
-             M.empty
-             M.empty
-             M.empty
-             M.empty
-             M.empty
-             M.empty
-             M.empty
-             []
-             M.empty
+             [] -- TODO:
+             [] -- TODO:
+             M.empty -- TODO:
+             M.empty -- TODO:
+             M.empty -- TODO:
+             M.empty -- TODO:
+             M.empty -- TODO:
+             M.empty -- TODO:
+             M.empty -- TODO:
+             ms
+             M.empty -- TODO:
     )
 
 {-|
