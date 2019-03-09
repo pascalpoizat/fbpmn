7df155f4bc42447f32148b5e2c893eea60607d80
pascalpoizat <pascal.poizat@lip6.fr>
2019-03-09 10:32:23 +0100

    feat: add high level processes and edges in BPMN import

--- src/Fbpmn/IO/Bpmn.hs
+++ src/Fbpmn/IO/Bpmn.hs
@@ -20,86 +20,71 @@ nA s = QName s Nothing Nothing
 nE :: String -> QName
 nE s = QName s (Just uri) (Just "bpmn")
 
-getId :: Element -> Maybe String
-getId e = findAttr (nA "id") e
+aOrElseB :: String -> String -> Element -> Maybe String
+aOrElseB an1 an2 e =
+  do
+    let av1 = findAttr (nA an1) e
+    let av2 = findAttr (nA an2) e
+    fromMaybe av2 (Just av1)
 
--- for the time being all start events are treated as NSE
--- (in the BPMN loading only, it is correct in the JSON loading)
-pSE :: QName -> Bool
-pSE = (== nE "startEvent")
+aOrElseB' :: String -> String -> String -> Element -> String
+aOrElseB' def an1 an2 e =
+    do
+      let av1 = findAttr (nA an1) e
+      let av2 = findAttr (nA an2) e
+      fromMaybe (fromMaybe def av2) av1
 
--- for the time being all end events are treated as NEE
--- (in the BPMN loading only, it is correct in the JSON loading)
-pEE :: QName -> Bool
-pEE = (== nE "endEvent")
+nameOrElseId :: Element -> Maybe String
+nameOrElseId = aOrElseB "name" "id"
 
-pOR :: QName -> Bool
-pOR = (== nE "inclusiveGateway")
-
-pAND :: QName -> Bool
-pAND = (== nE "parallelGateway")
-
-pXOR :: QName -> Bool
-pXOR = (== nE "exclusiveGateway")
-
-pG :: QName -> Bool
-pG q = pOR q || pAND q || pXOR q
-
-pST :: QName -> Bool
-pST = (== nE "sendTask")
-
-pRT :: QName -> Bool
-pRT = (== nE "receiveTask")
-
-pAT :: QName -> Bool
-pAT = (== nE "task")
-
-pT :: QName -> Bool
-pT q = pAT q || pST q || pRT q
-
-pN :: QName -> Bool
-pN q = pT q || pSE q || pEE q || pG q
-
-pMF :: QName -> Bool
-pMF = (== nE "messageFlow")
-
-pSF :: QName -> Bool
-pSF = (== nE "sequenceFlow")
-
-pE :: QName -> Bool
-pE q = pMF q || pSF q
+idOrNothing :: Element -> Maybe String
+idOrNothing = findAttr (nA "id")
 
 {-|
 An experimental BPMN reading.
 
+Requirements:
+- collaboration has a name
+- message flows have a name (used as message type)
+
 Enhancements:
-- remove duplicate message flow names
-- use messages instance of message flow names (if available)
+- remove duplicates in cMessageTypes
 -}
 decode :: [Content] -> Maybe BpmnGraph
 decode cs = do
-  elems <- pure $ onlyElems cs
-  c  <- listToMaybe $ concatMap (findElements (nE "collaboration")) elems
-  ns <- pure $ concatMap (filterElementsName pN) elems
-  es <- pure $ concatMap (filterElementsName pE) elems
-  name  <- getId c
-  -- ps <- pure $ findElements (nE "participant") c
-  mfs <- pure $ filterElementsName pMF c
-  ms <- sequence $ findAttr (nA "name") <$> mfs
-  Just
-    (mkGraph (toText name)
-             (catMaybes $ getId <$> ns) -- TODO: missing process nodes
-             (catMaybes $ getId <$> es)
-             M.empty -- TODO:
-             M.empty -- TODO:
-             M.empty -- TODO:
-             M.empty -- TODO:
-             M.empty -- TODO:
-             M.empty -- TODO:
-             M.empty -- TODO:
-             ms
-             M.empty -- TODO:
-    )
+    -- all elements
+    allElements <- pure $ onlyElems cs
+    -- collaboration (1st one to be found)
+    c <- listToMaybe $ concatMap (findChildren (nE "collaboration")) allElements
+    cName <- findAttr (nA "id") c
+    -- participants
+    cParticipants <- pure $ findChildren (nE "participant") c
+    cParticipantRefs <- sequence $ findAttr (nA "processRef") <$> cParticipants
+    cParticipantNames <- sequence $ aOrElseB "name" "processRef" <$> cParticipants
+    -- processes
+    allProcesses <- pure $ concatMap (findChildren (nE "process")) allElements
+    cProcesses <- pure $ filter (filterByReferences cParticipantRefs) allProcesses 
+    -- message flows and messages
+    cMessageFlows <- pure $ findChildren (nE "messageFlow") c
+    cMessageFlowIds <- sequence $ idOrNothing <$> cMessageFlows
+    cMessageTypes <- sequence $ nameOrElseId <$> cMessageFlows
+    -- graph
+    Just (mkGraph
+          (toText cName)
+          cParticipantNames -- TODO: completer avec tous les nodes
+          cMessageFlowIds -- TODO: completer avec tous les edges
+          M.empty -- TODO:
+          M.empty -- TODO:
+          M.empty -- TODO:
+          M.empty -- TODO:
+          M.empty -- TODO:
+          M.empty -- TODO:
+          M.empty -- TODO:
+          cMessageTypes
+          M.empty -- TODO:
+          )
+    where
+      filterByReferences = undefined 
 
 {-|
 Read a BPMN Graph from a BPMN file.
