dec1db3963b2c23fb9107f456a45b46381e9ad86
pascalpoizat <pascal.poizat@lip6.fr>
2019-03-10 17:48:39 +0100

    feat: add helper to lift Maybes in tuples

--- src/Fbpmn/Helper.hs
+++ src/Fbpmn/Helper.hs
@@ -8,5 +8,9 @@ mapMap g m = catMaybes $ mapMapElement g m <$> keys m
 mapMapElement :: Ord a => (a -> Maybe b -> Maybe c) -> Map a b -> a -> Maybe c
 mapMapElement g m k = g k (m !? k)
 
-filter' :: (Ord a) => [a] -> (Map a b) -> (Maybe b -> Bool) -> [a]
+filter' :: (Ord a) => [a] -> Map a b -> (Maybe b -> Bool) -> [a]
 filter' xs f p = filter p' xs where p' x = p $ f !? x
+
+tlift2 :: (Maybe a, Maybe b) -> Maybe (a, b)
+tlift2 (Just x, Just y) = Just (x, y)
+tlift2 (_     , _     ) = Nothing
