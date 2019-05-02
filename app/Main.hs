24658e5d1032165eb634802f049b6c1f09f2dc96
pascalpoizat <pascal.poizat@lip6.fr>
2019-05-02 11:48:27 +0300

    chore: move to v0.2.8

--- app/Main.hs
+++ app/Main.hs
@@ -29,7 +29,7 @@ data RCommand = RQuit        -- quit REPL
              -- | RSmt Text    -- save current graph as SMT
 
 fversion :: Text
-fversion = "0.2.7"
+fversion = "0.2.8"
 
 toolversion :: Text
 toolversion = fversion
