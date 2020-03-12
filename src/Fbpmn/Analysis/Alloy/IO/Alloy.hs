0d1057be31e8703d9091ac084c84e4279b766f0f
pascalpoizat <pascal.poizat@lip6.fr>
2020-03-12 18:07:32 +0100

    begin Alloy generation for nodes and edges

--- src/Fbpmn/Analysis/Alloy/IO/Alloy.hs
+++ src/Fbpmn/Analysis/Alloy/IO/Alloy.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE QuasiQuotes #-}
 module Fbpmn.Analysis.Alloy.IO.Alloy where
 
-import qualified Data.Text         as T
+import qualified Data.Text                     as T
 import           Fbpmn.Helper
 import           Fbpmn.BpmnGraph.Model
-import           NeatInterpolation (text)
+import           Fbpmn.Analysis.Alloy.Model
+import           NeatInterpolation              ( text )
 -- import           Data.List                      ( intercalate )
-import           Data.Map.Strict   ((!?), foldrWithKey)
+import           Data.Map.Strict                ( (!?)
+                                                , foldrWithKey
+                                                )
 
 {-|
 Write a BPMN Graph to an Alloy file.
@@ -20,42 +23,105 @@ Transform a BPMN Graph to an Alloy specification.
 encodeBpmnGraphToAlloy :: BpmnGraph -> Text
 encodeBpmnGraphToAlloy g =
   unlines
-    $   [ encodeBpmnGraphHeaderToAlloy          -- header
-        , encodeNodes                           -- experimental
-        , encodeTimerEventDefinitions           -- experimental
-        , encodeBpmnGraphFooterToAlloy          -- footer
+    $   [ encodeBpmnGraphHeaderToAlloy          --    
+        , encodeNodes
+        , encodeEdges
+        , encodeBpmnGraphFooterToAlloy vs
         ]
     <*> [g]
+    where
+      vs = [ AlloyVerification Check Safety 15
+           , AlloyVerification Check SimpleTermination 9
+           , AlloyVerification Check CorrectTermination 9
+           , AlloyVerification Run Safety 11]
 
 encodeBpmnGraphHeaderToAlloy :: BpmnGraph -> Text
-encodeBpmnGraphHeaderToAlloy g =
-  [text|
-  ---------------- MODULE $n ----------------
-  module $n
+encodeBpmnGraphHeaderToAlloy _ = [text|
+  open PWSSyntax
+  open PWSProp
 
   |]
-  where
-    n = name g
 
-encodeBpmnGraphFooterToAlloy :: BpmnGraph -> Text
-encodeBpmnGraphFooterToAlloy g =
-  [text|
-  ---------------- END MODULE $n ------------
-  |]
+encodeBpmnGraphFooterToAlloy :: [AlloyVerification] -> BpmnGraph -> Text
+encodeBpmnGraphFooterToAlloy vs _ = unlines $ verificationToAlloy <$> vs
+
+verificationToAlloy :: AlloyVerification -> Text
+verificationToAlloy v = [text|$tact {$tprop} for 0 but $tnb State|]
   where
-    n = name g
+    tact = case action v of
+      Run -> "run"
+      Check -> "check"
+    tprop = case property v of
+      Safety -> "Safe"
+      SimpleTermination -> "SimpleTermination"
+      CorrectTermination -> "CorrectTermination"
+    tnb = show $ nb v
 
 encodeNodes :: BpmnGraph -> Text
 encodeNodes g = [text|
-  $sn
+  $sns
+  |]
+ where
+  sns = unlines $ nodeToAlloy <$> nodes g
+  nodeToAlloy n = [text|
+      one sig $nname extends $ntype {
+        $ncontents
+      }
+      |]
+   where
+    nname = toText n
+    ntype = maybe "" nodeTypeToAlloy (catN g !? n)
+    ncontents = ""
+
+encodeEdges :: BpmnGraph -> Text
+encodeEdges g = [text|
+  $ses
   |]
   where
-    sn = unlines $ nodeToAlloy <$> ns
-    ns = nodes g
-    nodeToAlloy n = [text|$ssn :: $tn|]
+    ses = unlines $ edgeToAlloy <$> edges g
+    edgeToAlloy e = [text|
+      one sig $ename extends $etype {
+        $econtents
+      }
+      |]
       where
-        ssn = show n
-        tn = maybe "type not known" show $ catN g !? n
+        ename = toText e
+        etype = maybe "" edgeTypeToAlloy (catE g !? e)
+        econtents = ""
+
+nodeTypeToAlloy :: NodeType -> Text
+nodeTypeToAlloy AbstractTask                  = "AbstractTask"
+-- start
+nodeTypeToAlloy NoneStartEvent                = "NoneStartEvent"
+-- end
+nodeTypeToAlloy NoneEndEvent                  = "NoneEndEvent"
+nodeTypeToAlloy TerminateEndEvent             = "TerminateEndEvent"
+-- gateways
+nodeTypeToAlloy XorGateway                    = "ExclusiveOr"
+nodeTypeToAlloy OrGateway                     = "InclusiveOr"
+nodeTypeToAlloy AndGateway                    = "Parallel"
+nodeTypeToAlloy EventBasedGateway             = "EventBased"
+-- structure
+nodeTypeToAlloy SubProcess                    = "SubProcess"
+nodeTypeToAlloy Process                       = "Process"
+-- communication
+nodeTypeToAlloy MessageStartEvent             = "MessageStartEvent"
+nodeTypeToAlloy SendTask                      = "SendTask"
+nodeTypeToAlloy ReceiveTask                   = "ReceiveTask"
+nodeTypeToAlloy ThrowMessageIntermediateEvent = "ThrowMessageIntermediateEvent"
+nodeTypeToAlloy CatchMessageIntermediateEvent = "CatchMessageIntermediateEvent"
+nodeTypeToAlloy MessageBoundaryEvent          = "MessageBoundaryEvent"
+nodeTypeToAlloy MessageEndEvent               = "MessageEndEvent"
+-- time
+nodeTypeToAlloy TimerStartEvent               = "TimerStartEvent"
+nodeTypeToAlloy TimerIntermediateEvent        = "TimerIntermediateEvent"
+nodeTypeToAlloy TimerBoundaryEvent            = "TimerBoundaryEvent"
+
+edgeTypeToAlloy :: EdgeType -> Text
+edgeTypeToAlloy NormalSequenceFlow      = "NormalSeqFlow"
+edgeTypeToAlloy ConditionalSequenceFlow = "ConditionalSeqFlow"
+edgeTypeToAlloy DefaultSequenceFlow     = "DefaultSeqFlow"
+edgeTypeToAlloy MessageFlow             = "MessageFlow"
 
 encodeTimerEventDefinitions :: BpmnGraph -> Text
 encodeTimerEventDefinitions g = [text|
