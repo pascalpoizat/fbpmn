5bcd89344204e96c73ef883ac433d296a88dec84
malinvaudpaul <paul.malinvaud@lip6.fr>
2021-06-22 10:51:48 +0200

    feat: dialogue react on about.onClick

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,18 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
+import Dialog from "@material-ui/core/Dialog";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [currentVersion, setCurrentVersion] = useState(0);
+let version;
 
-  useEffect(() => {
-    fetch('/version').then(res => res.json()).then(data => {
-      setCurrentVersion(data.version);
-    });
-  }, []);
+function setVersion() {
+  fetch('/version').then(res => res.json()).then(data => {
+    version = data.major + "." + data.minor + "." + data.patch
+  });
+}
+
+function App() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickToOpen = () => {
+    setOpen(true);
+  };
+
+  const handleToClose = () => {
+    setOpen(false);
+  };
+
+  setVersion();
 
   return (
-    "fBPMN, v" + currentVersion
+    <div>
+      <a id="about-nav" onClick={handleClickToOpen}>About</a>
+      <Dialog open={open} onClose={handleToClose}>
+        <DialogTitle>fBPMN v{version}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Version {version}
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 }
 
--- web/frontend/src/index.js
+++ web/frontend/src/index.js
@@ -4,12 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('title')
-);
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
