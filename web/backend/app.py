4d511b2538358a0070e2409162cc429944539640
malinvaudpaul <paul.malinvaud@lip6.fr>
2021-06-23 10:25:10 +0200

    creation of bpmn file with the good name/content in the API

--- web/backend/app.py
+++ web/backend/app.py
@@ -1,13 +1,15 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from flask_mysqldb import MySQL
 import subprocess
 import os
+import re
 
 app = Flask(__name__)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+cors = CORS(app, resources={
+            r"/verifications": {"origins": "http://localhost:3000"}})
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -21,8 +23,8 @@ def version():
     version = subprocess.check_output(
         "fbpmn version", shell=True, universal_newlines=True)
     version = version.split('.')
-    major = version[0]
-    minor = version[1]
+    major = int(version[0])
+    minor = int(version[1])
     patch = version[2].splitlines()
     # insertion dans db
     '''
@@ -34,25 +36,36 @@ def version():
     '''
     return {'major': major,
             'minor': minor,
-            'patch': patch[0]}
+            'patch': int(patch[0])}
 
 
 @app.route('/verifications', methods=['GET', 'POST'])
-@cross_origin()
+@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def verifications():
     if request.method == 'POST':
-        model = (request.json['model'])
+        # with request.data only, a b' ' appears to indicate the string is binary
+        model = str(request.data.decode('UTF-8'))
+        name = (re.search(r'<bpmn:collaboration id="(.+)">', model)).group(1)
         # to conserve the "" in the bpmn file
         model_for_file = model.replace('\"', '\\"\"')
-        os.system(f'echo "{model_for_file}" > /tmp/e033MBE.bpmn')
-        id = 12
-        ref = 0
-        results = 1
-        os.system("fbpmn-check /tmp/e033MBE.bpmn")
-        return {'id': id,
-                'ref': ref,
-                'model': model,
-                'results': results}
+        os.system(f'echo "{model_for_file}" > /tmp/{name}.bpmn')
+        os.system(f'fbpmn-check /tmp/{name}.bpmn')
+        cursor = mysql.connection.cursor()
+        cursor.execute(
+            f' INSERT INTO Verification (model_name, model, `status`) VALUES (%s,%s,"PENDING") % ({name}, {model})')
+        mysql.connection.commit()
+        cursor.close()
+        # attente fin de vérif
+        '''
+        cursor.execute(
+            " UPDATE Verification VALUES(%d,%d,%d)" % (int(major), int(minor), int(patch[0])))
+        mysql.connection.commit()
+        '''
+        # insertion lignes verificationResults
+        # MAJ ligne verifs
+        # cursor.close()
+        # supprime fichier et dossier temp
+        return {'id': 0}
     else:
         return {'id': 0}
 
