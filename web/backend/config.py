91006a4f265124d94061d698f556f52c5c3cfaaa
malinvaudpaul <paul.malinvaud@lip6.fr>
2021-07-01 13:55:37 +0200

    add db (mySQL) container

--- web/backend/config.py
+++ web/backend/config.py
@@ -1,4 +1,12 @@
+import os
+
+
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql://admin:Malinva5d@localhost/fBPMN_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@{}/{}'.format(
+        os.getenv('DB_USER', 'admin'),
+        os.getenv('DB_PASSWORD', ''),
+        os.getenv('DB_HOST', 'mysql'),
+        os.getenv('DB_NAME', 'fBPMN_db')
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_HEADERS = 'Content-Type'
