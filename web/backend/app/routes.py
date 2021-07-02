e1c8697affb65f58816e07272013cafe298ea67c
malinvaudpaul <paul.malinvaud@lip6.fr>
2021-07-02 16:16:27 +0200

    refs in api

--- web/backend/app/routes.py
+++ web/backend/app/routes.py
@@ -1,9 +1,11 @@
 from flask import request, jsonify
 import time
 from app import app, db
-from app.models import Application, Version
+from app.models import Application, Version, get_workdir
 
 a = Application()
+
+# TODO refactorer -> une fonction serialize() pour éviter répétition de jsonify
 
 
 @app.route('/api/time')
@@ -25,6 +27,7 @@ def get_all_models():
     models_json = []
     for m in models:
         m.__dict__['taille'] = len(m.__dict__['content'])
+        m.__dict__['verification'] = f'/verifications/{m.verification[0].id}'
         del m.__dict__['_sa_instance_state'], m.__dict__['content']
         models_json.append(m.__dict__)
     return jsonify(models_json)
@@ -37,17 +40,25 @@ def verifications():
         model = str(request.data.decode('UTF-8'))
         v = a.create_verification()
         m = v.create_model(model)
-        workir = v.launch_check(m.name)
-        xx = v.results_list(workir, m.name)
+        output = v.launch_check(m.name)
+        workdir = get_workdir(output)
+        xx = v.results_list(workdir, m.name)
         del m, v
-        return "tout roule"
+        return output
     else:
         verifications = Application.get_all_verifications()
         verifications_json = []
         for v in verifications:
+            results_json = []
+            for r in v.results:
+                results_json.append(f'/results/{r.id}')
             del v.__dict__['_sa_instance_state'], v.__dict__[
-                'pub_date'], v.__dict__['status']
+                'pub_date']
+            v.__dict__['status'] = str(v.status.name)
+            v.__dict__['model_id'] = f'/models/{v.model_id}'
+            v.__dict__['results'] = results_json
             verifications_json.append(v.__dict__)
+
         return jsonify(verifications_json)
 
 
@@ -58,8 +69,10 @@ def get_all_results():
     results = Application.get_all_results()
     results_json = []
     for r in results:
-        del r.__dict__['_sa_instance_state'], r.__dict__[
-            'communication'], r.__dict__['property']
+        del r.__dict__['_sa_instance_state']
+        r.__dict__['communication'] = str(r.communication.name)
+        r.__dict__['property'] = str(r.property.name)
+        r.__dict__['verification_id'] = f'verifications/{r.verification_id}'
         results_json.append(r.__dict__)
     return jsonify(results_json)
 
@@ -74,33 +87,42 @@ def get_model_by_id(id):
 @app.route('/verifications/<id>', methods=['GET'])
 def get_verification_by_id(id):
     v = Application.get_verification_by_id(id)
-    print(len(v.results))
-    return jsonify(id=v.id, date=v.pub_date, model=v.model_id, output=v.output, results=f'/results/{v.results[0].id}')
+    results_json = []
+    for r in v.results:
+        results_json.append(f'/results/{r.id}')
+    return jsonify(id=v.id, status=str(v.status.name),
+                   date=v.pub_date, model=f'models/{v.model.id}', output=v.output, results=results_json)
 
 
 @app.route('/results/<id>', methods=['GET'])
 def get_result_by_id(id):
     r = Application.get_result_by_id(id)
-    return jsonify(id=r.id, value=r.value, verification=f'verifications/{r.verification.id}')
+    return jsonify(id=r.id, communication=str(r.communication.name),
+                   property=str(r.property.name), value=r.value, verification=f'verifications/{r.verification.id}')
 
 
 @app.route('/verifications/<id>/model', methods=['GET'])
 def get_model_by_verification(id):
     model_id = (Application.get_verification_by_id(id)).get_model()
-    m = get_model_by_id(model_id)
-    return jsonify(id=m.id, name=m.name,
-                   taille=len(m.content))
+    return get_model_by_id(model_id)
 
 
 @app.route('/verifications/<id>/results', methods=['GET'])
 def get_results_by_verification(id):
-    verification_id = Application.get_verification_by_id(id)
-    # TODO use relationship or a new Application's method
+    verification = Application.get_verification_by_id(id)
+    results_json = []
+    for r in verification.results:
+        del r.__dict__['_sa_instance_state']
+        r.__dict__['communication'] = str(r.communication.name)
+        r.__dict__['property'] = str(r.property.name)
+        r.__dict__['verification_id'] = f'verifications/{r.verification_id}'
+        results_json.append(r.__dict__)
+    return jsonify(results_json)
 
 
 @app.route('/results/<id>/verification', methods=['GET'])
 def get_verification_by_result(id):
     verification_id = (Application.get_result_by_id(id)).get_verification()
     v = Application.get_verification_by_id(verification_id)
-    return jsonify(id=v.id, status=str(v.status),
+    return jsonify(id=v.id, status=str(v.status.name),
                    date=v.pub_date, model=v.model_id)
