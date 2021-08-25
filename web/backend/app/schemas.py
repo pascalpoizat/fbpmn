8f132bfdc16c2695ed3f63972cb0114f93a4244e
malinvaudpaul <paul.malinvaud@lip6.fr>
2021-08-25 14:12:14 +0200

    coherence with the new structure of API in schemas

--- web/backend/app/schemas.py
+++ web/backend/app/schemas.py
@@ -1,7 +1,9 @@
 from app import ma
-from app.models import Status, Value, Constraints, CounterExample, Model, UserDefs, UserProps, Verification, Result
+from app.models import Status, Constraints, CounterExample, Model, UserDefs, UserProps, Verification, Result
 from app.context import Communication, Property
 from marshmallow_enum import EnumField
+
+VERIFICATION_BY_ID = "api.verifications_verification_by_id"
 
 
 class ModelSchema(ma.SQLAlchemyAutoSchema):
@@ -9,28 +11,28 @@ class ModelSchema(ma.SQLAlchemyAutoSchema):
         model = Model
         include_relationships = True
         load_instance = True
-    verification = ma.HyperlinkRelated("get_verification_by_id")
+    verification = ma.HyperlinkRelated(VERIFICATION_BY_ID)
 
 
 class UserDefsSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = UserDefs
         include_relationships = True
-    verification = ma.HyperlinkRelated("get_verification_by_id")
+    verification = ma.HyperlinkRelated(VERIFICATION_BY_ID)
 
 
 class UserPropsSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = UserProps
         include_relationships = True
-    verification = ma.HyperlinkRelated("get_verification_by_id")
+    verification = ma.HyperlinkRelated(VERIFICATION_BY_ID)
 
 
 class ConstraintsSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Constraints
         include_relationships = True
-    verification = ma.HyperlinkRelated("get_verification_by_id")
+    verification = ma.HyperlinkRelated(VERIFICATION_BY_ID)
 
 
 class VerificationSchema(ma.SQLAlchemyAutoSchema):
@@ -40,11 +42,11 @@ class VerificationSchema(ma.SQLAlchemyAutoSchema):
 
     status = EnumField(Status)
     pub_date = ma.DateTime(format='%Y-%m-%dT%H:%M')
-    results = ma.List(ma.HyperlinkRelated("get_result_by_id"))
-    model = ma.HyperlinkRelated("get_model_by_id")
-    userdefs = ma.HyperlinkRelated("get_userdefs_by_id")
-    userprops = ma.HyperlinkRelated("get_userprops_by_id")
-    constraints = ma.HyperlinkRelated("get_constraints_by_id")
+    results = ma.List(ma.HyperlinkRelated("api.results_result_by_id"))
+    model = ma.HyperlinkRelated("api.models_model_by_id")
+    userdefs = ma.HyperlinkRelated("api.userdefs_user_defs_by_id")
+    userprops = ma.HyperlinkRelated("api.userprops_user_props_by_id")
+    constraints = ma.HyperlinkRelated("api.constraints_constraints_by_id")
 
 
 class ResultSchema(ma.SQLAlchemyAutoSchema):
@@ -54,12 +56,11 @@ class ResultSchema(ma.SQLAlchemyAutoSchema):
 
     communication = EnumField(Communication)
     property = EnumField(Property)
-    verification = ma.HyperlinkRelated("get_verification_by_id")
-    counter_example = ma.HyperlinkRelated("get_counter_examples_by_id")
+    verification = ma.HyperlinkRelated(VERIFICATION_BY_ID)
 
 
 class CounterExampleSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = CounterExample
         include_realtionships = True
-    result = ma.HyperlinkRelated("get_result_by_id")
+    result = ma.HyperlinkRelated("api.results_result_by_id")
