73ede1e9adc7a0b3e81239aecc16a9b2e6ae0964
malinvaudpaul <paul.malinvaud@lip6.fr>
2021-08-26 16:09:53 +0200

    try to test directly the parseJSON component's method

--- web/frontend/src/components/CounterExample/CounterExample.test.js
+++ web/frontend/src/components/CounterExample/CounterExample.test.js
@@ -1,8 +1,15 @@
+import React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
+import CounterExample from "./CounterExample.js";
+configure({ adapter: new Adapter() });
+
 const stepsExpected = require("./expected/e001ClientSupplier.Network02FifoPair.Prop03Sound.js");
 
-const parseJSON = require("./CounterExampleFunctions.js");
-
 test("compare js from log2html with js from parseJSON of CounterExample.js", () => {
+  const wrapper = shallow(<CounterExample></CounterExample>);
   let jsonfile = require("./observed/e001ClientSupplier.Network02FifoPair.Prop03Sound.json");
-  expect(parseJSON(JSON.stringify(jsonfile.lcex))).toBe(stepsExpected());
+  expect(wrapper.instance().parseJSON(JSON.stringify(jsonfile.lcex))).toBe(
+    stepsExpected()
+  );
 });
